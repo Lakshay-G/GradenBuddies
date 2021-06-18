e4a9401b1e9285e7920bbe52eb65266bbf4b6108
Shashank <72060531+shashank026@users.noreply.github.com>
2021-06-19 01:40:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HashLoader from "react-spinners/HashLoader";
+
+function App() {
+  return (
+    <div className="App">
+      <HashLoader size ={150} color = {"36D1D7"} loading />
+    </div>
+  );
+}
+
+export default App;
