7e51a5e21234c23beb2e2f146c3e5769ff7634e5
Virtibot <59942900+Lakshay-G@users.noreply.github.com>
2021-06-20 06:55:43 -0400

    Navlink Re-inserted
    -Added {NavLink}
    - Uncommented Navlink code
    - Added the 2 buttons, Timeline and Info buttons
    - Made sure they direct to correct webpages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import web from '../assets/plant1.jpeg';
 import plant from '../assets/plant2.jpeg';
 import plant2 from '../assets/plant3.jpeg';
@@ -23,9 +23,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Abelia</h5>
                                     <p class="card-text">Plant during milder months of spring or fall to avoid heat or cold stress.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
 
@@ -34,9 +37,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Abutilon</h5>
                                     <p class="card-text">Soil must be constantly moist. Use warm water. In the winter, watering is limited.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
 
@@ -45,9 +51,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Agave</h5>
                                     <p class="card-text">Summer is rare, in winter – limited to a minimum</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
                             <div class="card" >
@@ -55,9 +64,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Browallia</h5>
                                     <p class="card-text">	Frequent, in the summer in the flowering period every 3 days. In winter it is moderate.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                        Info
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
@@ -68,9 +80,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Camellia</h5>
                                     <p class="card-text">From spring to autumn every two weeks with liquid fertilizer without lime. You can use fertilizer for orchids. Organic fertilizers are useful.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
 
@@ -79,9 +94,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Colocasia</h5>
                                     <p class="card-text">Slightly acidic light fertile soil. Equal parts of sand, humus, peat and turf. Required drainage.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
 
@@ -90,9 +108,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Eupatorium</h5>
                                     <p class="card-text">During the growing season every 10-15 days with liquid fertilizer. Every 3 weeks it is useful to use organic fertilizer. Fertile soil is required.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
 
@@ -101,9 +122,12 @@ const Home = () => {
                                 <div class="card-body">
                                     <h5 class="card-title">Hippeastrum</h5>
                                     <p class="card-text">Fed with liquid fertilizer every 3-4 weeks with the appearance of flower arrows. Top dressing is stopped in autumn with yellowing of the leaves.</p>
-                                    {/* <NavLink to="/Events" className="btn btn-dark" role="button">
-                                    Read
-                                </NavLink> */}
+                                    <NavLink to="/Timeline" className="btn btn-dark" role="button">
+                                    Timeline
+                                    </NavLink>
+                                    <NavLink to="/Info" className="btn btn-light mx-2" role="button">
+                                    Info
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
